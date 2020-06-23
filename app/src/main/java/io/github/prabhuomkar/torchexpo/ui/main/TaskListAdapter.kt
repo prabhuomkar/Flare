985714961786ff89c34e17a1f11e797e31010a66
prabhuomkar <prabhuomkar@pm.me>
2020-06-23 18:56:21 +0530

    Added assets updates to UI

---
+++ app/src/main/java/io/github/prabhuomkar/torchexpo/ui/main/TaskListAdapter.kt
@@ -0,0 +1,5 @@
+package io.github.prabhuomkar.torchexpo.ui.main
+
+class TaskListAdapter {
+    // TODO: Add Tasks Recycler View Adapter
+}
---
+++ app/src/main/java/io/github/prabhuomkar/torchexpo/ui/task/ModelListAdapter.kt
@@ -0,0 +1,5 @@
+package io.github.prabhuomkar.torchexpo.ui.task
+
+class ModelListAdapter {
+    // TODO: Add Models Recycler View Adapter
+}
