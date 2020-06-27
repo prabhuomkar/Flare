fb534fba92c89c354aa860dfdf914ca64196986c
prabhuomkar <prabhuomkar@pm.me>
2020-06-27 20:34:15 +0530

    placeholder image added

--- app/src/main/java/io/github/prabhuomkar/torchexpo/ui/BindingUtils.kt
+++ app/src/main/java/io/github/prabhuomkar/torchexpo/ui/BindingUtils.kt
@@ -3,10 +3,12 @@ package io.github.prabhuomkar.torchexpo.ui
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import io.github.prabhuomkar.torchexpo.R
 
 @BindingAdapter("image")
 fun loadImage(view: ImageView, url: String) {
     Glide.with(view)
         .load(url)
+        .placeholder(R.mipmap.ic_launcher)
         .into(view)
 }
