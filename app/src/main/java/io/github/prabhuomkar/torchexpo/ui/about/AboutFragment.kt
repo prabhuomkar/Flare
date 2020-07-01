a30a803ea7f92b8eec454460c9a8ab189b5738bd
prabhuomkar <prabhuomkar@pm.me>
2020-07-01 12:20:35 +0530

    About Fragment refactoring

--- app/src/main/java/io/github/prabhuomkar/torchexpo/ui/about/AboutFragment.kt
+++ app/src/main/java/io/github/prabhuomkar/torchexpo/ui/about/AboutFragment.kt
@@ -6,15 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
-import io.github.prabhuomkar.torchexpo.BuildConfig
-import io.github.prabhuomkar.torchexpo.R
 import io.github.prabhuomkar.torchexpo.databinding.AboutFragmentBinding
 
 class AboutFragment : Fragment() {
 
-    private lateinit var viewModel: AboutViewModel
     private var _binding: AboutFragmentBinding? = null
     private val binding get() = _binding!!
 
@@ -24,31 +20,21 @@ class AboutFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = AboutFragmentBinding.inflate(inflater, container, false)
+        binding.fragment = this
         return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding.versionTextView.text =
-            requireContext().getString(R.string.app_version, BuildConfig.VERSION_NAME)
-        binding.licensesTextView.setOnClickListener { _ ->
-            startActivity(
-                Intent(
-                    activity,
-                    OssLicensesMenuActivity::class.java
-                )
-            )
-        }
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(AboutViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    fun openLicenses(view: View) {
+        view.context.startActivity(
+            Intent(
+                activity,
+                OssLicensesMenuActivity::class.java
+            )
+        )
     }
 }
--- app/src/main/java/io/github/prabhuomkar/torchexpo/ui/about/AboutViewModel.kt
+++
@@ -1,7 +0,0 @@
-package io.github.prabhuomkar.torchexpo.ui.about
-
-import androidx.lifecycle.ViewModel
-
-class AboutViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
