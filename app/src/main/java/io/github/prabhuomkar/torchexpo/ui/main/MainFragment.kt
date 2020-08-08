9405b3eca664574b650c80dcd5402cb815ac196b
prabhuomkar <prabhuomkar@pm.me>
2020-08-08 12:25:32 +0530

    Refactoring and clean up for starting NLP

--- app/src/main/java/io/github/prabhuomkar/torchexpo/ui/main/MainFragment.kt
+++ app/src/main/java/io/github/prabhuomkar/torchexpo/ui/main/MainFragment.kt
@@ -1,7 +1,6 @@
 package io.github.prabhuomkar.torchexpo.ui.main
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,8 +10,6 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import io.github.prabhuomkar.torchexpo.databinding.MainFragmentBinding
-import io.github.prabhuomkar.torchexpo.util.TensorUtil
-import org.pytorch.Tensor
 
 class MainFragment : Fragment() {
 
@@ -40,19 +37,6 @@ class MainFragment : Fragment() {
                 viewModel.getTasksFromNetwork()
             }
         })
-
-        val tensor = Tensor.fromBlob(
-            floatArrayOf(
-                0.0701F, 0.6310F, 0.8519F, 0.5257F, 0.2673F,
-                0.9703F, 0.2276F, 0.1715F, 0.5346F, 0.2133F, 0.8320F, 0.9678F
-            ), longArrayOf(3, 2, 2)
-        )
-            .dataAsFloatArray
-        val result = TensorUtil.argmax(tensor, 3, 2, 2)
-        Log.v("TEST", result[0].toString())
-        Log.v("TEST", result[1].toString())
-        Log.v("TEST", result[2].toString())
-        Log.v("TEST", result[3].toString())
 
         binding.refreshTaskList.setOnRefreshListener {
             viewModel.getTasksFromNetwork()
